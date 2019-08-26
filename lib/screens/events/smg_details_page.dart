049e86214d05872ce6a3cd0571f6f1629667b12e
Bharat Kathi <bharat1031@gmail.com>
2019-08-25 20:43:16 -0700

    Updated SMG Page

--- lib/screens/events/smg_details_page.dart
+++ lib/screens/events/smg_details_page.dart
@@ -36,30 +36,61 @@ class _StockDetailsPageState extends State<StockDetailsPage> {
                       elevation: 6.0,
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
                       color: currCardColor,
-                      child: new Column(
-                        children: <Widget>[
-                          new Padding(padding: EdgeInsets.all(4.0),),
-                          new Text(
-                            selectedEvent.eventName,
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)
-                          ),
-                          new ListTile(
-                            title: new Image.asset('images/1to3participants.png'),
-                            trailing: new Text(email, style: TextStyle(fontSize: 16.0, fontFamily: "Product Sans")),
-                          ),
-                          new ListTile(
-                            title: new Text("Role", style: TextStyle(fontFamily: "Product Sans",)),
-                            trailing: new Text(role, style: TextStyle(fontSize: 16.0, fontFamily: "Product Sans")),
-                          ),
-                          new ListTile(
-                            title: new Text("Chaperone Group", style: TextStyle(fontFamily: "Product Sans",)),
-                            trailing: new Text(chapGroupID, style: TextStyle(fontSize: 16.0, fontFamily: "Product Sans")),
-                          ),
-                          new ListTile(
-                            title: new Text("User ID", style: TextStyle(fontFamily: "Product Sans",)),
-                            trailing: new Text(userID, style: TextStyle(fontSize: 14.0, fontFamily: "Product Sans")),
-                          ),
-                        ],
+                      child: new Container(
+                        padding: EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0, bottom: 16.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new Padding(padding: EdgeInsets.all(4.0),),
+                            new Text(
+                              selectedEvent.eventName,
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)
+                            ),
+                            new Container(
+                              width: double.infinity,
+                              height: 100.0,
+                              child: new Row(
+                                children: <Widget>[
+                                  new Expanded(
+                                    flex: 3,
+                                    child: new Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                        new Text(
+                                          "1-3",
+                                          style: TextStyle(fontSize: 35.0),
+                                        ),
+                                        new Text(
+                                          "Participants",
+                                          style: TextStyle(fontSize: 15.0),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  new Expanded(
+                                    flex: 3,
+                                    child: new Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                        new Text(
+                                          "1-3",
+                                          style: TextStyle(fontSize: 35.0),
+                                        ),
+                                        new Text(
+                                          "Participants",
+                                          style: TextStyle(fontSize: 15.0),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            new Text(
+                              "Participants in the Stock Market Game develop and manage an investment portfolio. Each participating team manages all aspects of the portfolio including stock selection, buying and selling. The goal of the competition is to increase the value of the beginning portfolio.",
+                              style: TextStyle(fontSize: 15.0),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   )
